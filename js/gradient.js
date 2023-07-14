b3c1c1fc3205465bdc9ea5a3c6d9f29cc0b00349
Shahrukh1996 <saifiiam@gmail.com>
2023-07-14 14:04:40 +0530

    modify gradient.js

--- js/gradient.js
+++ js/gradient.js
@@ -1,60 +1,140 @@
-var colors = new Array(
-    [62,35,255],
-    [60,255,60],
-    [255,35,98],
-    [45,175,230],
-    [255,0,255],
-    [255,128,0]);
+// var colors = new Array(
+//     [62,35,255],
+//     [60,255,60],
+//     [255,35,98],
+//     [45,175,230],
+//     [255,0,255],
+//     [255,128,0]);
   
-  var step = 0;
-  //color table indices for: 
-  // current color left
-  // next color left
-  // current color right
-  // next color right
-  var colorIndices = [0,1,2,3];
+//   var step = 0;
+//   //color table indices for: 
+//   // current color left
+//   // next color left
+//   // current color right
+//   // next color right
+//   var colorIndices = [0,1,2,3];
   
-  //transition speed
-  var gradientSpeed = 0.004;
+//   //transition speed
+//   var gradientSpeed = 0.004;
   
-  function updateGradient()
-  {
+//   function updateGradient()
+//   {
     
-    if ( $===undefined ) return;
+//     if ( $===undefined ) return;
     
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
+//   var c0_0 = colors[colorIndices[0]];
+//   var c0_1 = colors[colorIndices[1]];
+//   var c1_0 = colors[colorIndices[2]];
+//   var c1_1 = colors[colorIndices[3]];
   
+//   var istep = 1 - step;
+//   var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+//   var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+//   var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+//   var color1 = "rgb("+r1+","+g1+","+b1+")";
+  
+//   var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+//   var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+//   var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+//   var color2 = "rgb("+r2+","+g2+","+b2+")";
+  
+//    $('#gradient').css({
+//      background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+//       background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+    
+//     step += gradientSpeed;
+//     if ( step >= 1 )
+//     {
+//       step %= 1;
+//       colorIndices[0] = colorIndices[1];
+//       colorIndices[2] = colorIndices[3];
+      
+//       //pick two new target color indices
+//       //do not pick the same as the current one
+//       colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+//       colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+      
+//     }
+//   }
+  
+//   setInterval(updateGradient,10);
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// var colors = [
+//   [51, 51, 51], // dark gray color
+//   [255, 255, 255] // white color
+// ];
+
+// var step = 0;
+// var colorIndices = [0, 1];
+// var gradientSpeed = 0.004;
+
+// function updateGradient() {
+//   if ($ === undefined) return;
+
+//   var c0 = colors[colorIndices[0]];
+//   var c1 = colors[colorIndices[1]];
+
+//   var istep = 1 - step;
+//   var r = Math.round(istep * c0[0] + step * c1[0]);
+//   var g = Math.round(istep * c0[1] + step * c1[1]);
+//   var b = Math.round(istep * c0[2] + step * c1[2]);
+//   var color = "rgb(" + r + "," + g + "," + b + ")";
+
+//   $('#gradient').css({
+//     background: color,
+//     backgroundImage: "repeating-linear-gradient(-45deg, rgba(0, 0, 0, 0.1) 25%, transparent 25%, transparent 50%, rgba(0, 0, 0, 0.1) 50%, rgba(0, 0, 0, 0.1) 75%, transparent 75%, transparent)"
+//   });
+
+//   step += gradientSpeed;
+//   if (step >= 1) {
+//     step %= 1;
+//     colorIndices[0] = colorIndices[1];
+//     colorIndices[1] = (colorIndices[1] + 1) % colors.length;
+//   }
+// }
+
+// setInterval(updateGradient, 10);
+
+
+
+
+
+var colors = [
+  [40, 40, 40],   // dark gray color
+  [80, 80, 80]    // light black color
+];
+
+var step = 0;
+var colorIndices = [0, 1];
+var gradientSpeed = 0.004;
+
+function updateGradient() {
+  if ($ === undefined) return;
+
+  var c0 = colors[colorIndices[0]];
+  var c1 = colors[colorIndices[1]];
+
   var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb("+r1+","+g1+","+b1+")";
-  
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb("+r2+","+g2+","+b2+")";
-  
-   $('#gradient').css({
-     background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-    
-    step += gradientSpeed;
-    if ( step >= 1 )
-    {
-      step %= 1;
-      colorIndices[0] = colorIndices[1];
-      colorIndices[2] = colorIndices[3];
-      
-      //pick two new target color indices
-      //do not pick the same as the current one
-      colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-      
-    }
+  var r = Math.round(istep * c0[0] + step * c1[0]);
+  var g = Math.round(istep * c0[1] + step * c1[1]);
+  var b = Math.round(istep * c0[2] + step * c1[2]);
+  var color = "rgb(" + r + "," + g + "," + b + ")";
+
+  $('#gradient').css({
+    background: color
+  });
+
+  step += gradientSpeed;
+  if (step >= 1) {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[1] = (colorIndices[1] + 1) % colors.length;
   }
-  
-  setInterval(updateGradient,10);
+}
+
+setInterval(updateGradient, 10);
